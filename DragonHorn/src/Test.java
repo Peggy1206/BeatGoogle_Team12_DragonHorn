96dead1396b73babd1188ca0237c0161f02f593a
Peixuan Wang <chintan5327@gmail.com>
2018-12-28 18:32:37 +0800

    finish getting url

--- DragonHorn/src/Test.java
+++ DragonHorn/src/Test.java
@@ -3,30 +3,20 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Test {
-
-	static String search = "";
-	static HashMap<String,String> searchResult = new HashMap<String,String>();
 	
+	public static String keyword = "";
+	
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
 		while(scanner.hasNextLine()) {
-			String Keyword = scanner.next();
-			GoogleQuery googleQuery = new GoogleQuery(Keyword);
-			searchResult = googleQuery.query();
-			for(String item : searchResult.values()) {
-	         //get all url
+			keyword = scanner.next();
 			
-				for(int i = 0; i < searchResult.size(); i++) {
-					
-				}
-			}
-			 
 		}
+		scanner.close();
 		
 		
 		
-			scanner.close();
 	}
 }
